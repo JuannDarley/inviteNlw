8290245caf025b53650b25e0e034e1c31042dda5
JuannDarley <juandarley99@gmail.com>
2025-02-22 15:31:42 -0300

    create connection with db and config

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,10 @@ import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { fastify } from "fastify";
 import {
-  type ZodTypeProvider,
-  jsonSchemaTransform,
-  serializerCompiler,
-  validatorCompiler,
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
 } from "fastify-type-provider-zod";
 import { env } from "./env";
 import { subscribeToEventRoute } from "./routes/subscribe-to-event-routes";
@@ -17,25 +17,25 @@ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors, {
-  origin: "http://localhost:3000",
+	origin: "http://localhost:3000",
 });
 
 app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "NLW Connect",
-      version: "0.0.1",
-    },
-  },
-  transform: jsonSchemaTransform,
+	openapi: {
+		info: {
+			title: "NLW Connect",
+			version: "0.0.1",
+		},
+	},
+	transform: jsonSchemaTransform,
 });
 
 app.register(fastifySwaggerUi, {
-  routePrefix: "/docs",
+	routePrefix: "/docs",
 });
 
 app.register(subscribeToEventRoute);
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log("🚀⚓ HTTP server running! ⚓🚀");
+	console.log("🚀⚓ HTTP server running! ⚓🚀");
 });
