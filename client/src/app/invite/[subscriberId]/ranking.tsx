1c72ee4784a1c02a19d407ad609e6f75ce746e1b
JuannDarley <juandarley99@gmail.com>
2025-03-02 17:25:42 -0300

    update page ranking

--- client/src/app/invite/[subscriberId]/ranking.tsx
+++ client/src/app/invite/[subscriberId]/ranking.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
 import { getRanking } from '@/http/api'
-import cooper from '../../../assets/Medal_cooper.svg'
-import gold from '../../../assets/Medal_gold.svg'
-import silver from '../../../assets/Medal_silver.svg'
+import cooper from '../../../assets/medal-cooper.svg'
+import gold from '../../../assets/medal-gold.svg'
+import silver from '../../../assets/medal-silver.svg'
 
 export async function Ranking() {
   const { ranking } = await getRanking()
@@ -13,35 +13,43 @@ export async function Ranking() {
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
         Ranking de indicações
       </h2>
+
       <div className="space-y-4">
-        {ranking.map((item, index) => {
+        {ranking.map((rank, index) => {
           const rankingPosition = index + 1
 
           return (
             <div
-              key={item.id}
+              key={rank.id}
               className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
             >
-              <span className="text-sm text-gray-300 leading-none ">
-                <span className="font-semibold">{rankingPosition}°</span> |{' '}
-                {item.name}
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}&ordm;</span> |{' '}
+                {rank.name}
               </span>
-              <span className="font-heading text-2xl font-semibold text-gray-300 leading-none">
-                {item.score}
+
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {rank.score}
               </span>
 
               {rankingPosition === 1 && (
                 <Image src={gold} alt="" className="absolute top-0 right-8" />
               )}
+
               {rankingPosition === 2 && (
                 <Image src={silver} alt="" className="absolute top-0 right-8" />
               )}
+
               {rankingPosition === 3 && (
                 <Image src={cooper} alt="" className="absolute top-0 right-8" />
               )}
             </div>
           )
         })}
+
+        {ranking.length === 0 && (
+          <p className="text-gray-300">Nenhuma indicação registrada</p>
+        )}
       </div>
     </div>
   )
